f147f26b17b4394397c8339faf3936767ffa8939
Rohan Vishwakarma <rohanvishwakarma8261@gmail.com>
2026-01-19 03:24:23 +0530

    feat: integrate Clerk authentication and error handling in backend and frontend

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { authCallback, getMe } from "../controllers/authControllers";
+import { protectRoutes } from "../middleware/auth";
 
 const router = Router();
+
+router.get("/me", protectRoutes, getMe);
+router.post("/callback", authCallback);
 
 export default router;
