cc787277415348b38ef20a4496d542b88510c5e5
Rohan Vishwakarma <rohanvishwakarma8261@gmail.com>
2026-01-19 11:39:32 +0530

    feat: implement chat, message, and user retrieval routes with authentication

--- backend/src/routes/messageRoutes.ts
+++ backend/src/routes/messageRoutes.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { protectRoutes } from "../middleware/auth";
+import { getMessages } from "../controllers/messageControllers";
 
 const router = Router();
+
+router.get("/chat/:chatId", protectRoutes, getMessages);
 
 export default router;
